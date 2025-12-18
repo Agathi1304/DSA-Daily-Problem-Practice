fd88cf6d892658701176f8d5d080fe0bb903b573
agathiyan <agathitechwork@gmail.com>
2025-12-19 00:13:22 +0530

    ArrayProblems -> 19/12/2025

---
+++ src/Array_Problems/FirstMissingPositive_LC_41.java
@@ -0,0 +1,64 @@
+package Array_Problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * Given an unsorted integer array nums. Return the smallest positive integer that is not present in nums.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+Example 3:
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+ */
+
+
+public class FirstMissingPositive_LC_41 {
+
+	public static void main(String[] args) {
+		
+		int  nums[] = { 3,2,0};
+		System.out.print("find first missing positive number : "+missingposNo(nums));
+
+	}
+
+	private static int missingposNo(int[] nums) {
+		
+		Set<Integer> set = new HashSet<>();
+		
+		for(int k:nums) {
+			set.add(k);
+		}
+		
+		int res=1;
+		
+		while(set.contains(res)) {
+			res++;
+		}
+		
+		return res;
+	}
+
+}
---
+++ src/Array_Problems/PartitionArrayIntoThreePartsWithEqualSum_LC_1013.java
@@ -0,0 +1,71 @@
+package Array_Problems;
+
+/**
+ * 
+ * Given an array of integers arr, return true if we can partition the array into three non-empty parts with equal sums.
+
+Formally, we can partition the array if we can find indexes i + 1 < j with (arr[0] + arr[1] + ... + arr[i] == arr[i + 1] + arr[i + 2] + ... + arr[j - 1] == arr[j] + arr[j + 1] + ... + arr[arr.length - 1])
+
+ 
+
+Example 1:
+
+Input: arr = [0,2,1,-6,6,-7,9,1,2,0,1]
+Output: true
+Explanation: 0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+Example 2:
+
+Input: arr = [0,2,1,-6,6,7,9,-1,2,0,1]
+Output: false
+Example 3:
+
+Input: arr = [3,3,6,5,-2,2,5,1,-9,4]
+Output: true
+Explanation: 3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+ 
+
+Constraints:
+
+3 <= arr.length <= 5 * 104
+-104 <= arr[i] <= 104
+ */
+public class PartitionArrayIntoThreePartsWithEqualSum_LC_1013 {
+
+	public static void main(String[] args) {
+		
+		int arr[] =   { 0,2,1,-6,6,-7,9,1,2,0,1};
+		boolean ans=partitionThreePArtsWithequalSum(arr);
+		System.out.println("Partition withb three PArts with equal sum : "+ans);
+
+	}
+
+	private static boolean partitionThreePArtsWithequalSum(int[] arr) {
+		
+		int total=0;
+		//calculating total amount to find the common value between three partiotion value
+		for(int k:arr) {
+			total+=k;
+		}
+		
+		if(total%3!=0) return false;
+		
+		int target=total/3;
+		int count=0,currentSum=0;
+
+        // Traverse the array and count how many parts match target sum
+		for(int i=0;i<arr.length;i++)
+		{
+			currentSum+=arr[i];
+			
+			if(currentSum==target) {
+				count++;
+				currentSum=0;// reset for next part
+			}
+		}
+		
+		return count>=3;
+        // At least 3 parts should be found
+				
+	}
+
+}
